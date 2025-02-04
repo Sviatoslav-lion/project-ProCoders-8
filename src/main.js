64ab8f502d9aad27e3ba7c68058e6fa804544243
Sviatoslav <sviat.danko@gmail.com>
2025-02-05 00:13:02 +0200

    start

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import './js/01-header.js';
+import './js/02-hero.js';
+import './js/03-about_me.js';
+import './js/04-benefits.js';
+import './js/05-projects.js';
+import './js/06-faq.js';
+import './js/07-covers.js';
+import './js/08-reviews.js';
+import './js/09-work_together.js';
+import './js/10-menu.js';
+import './js/11-pop-up.js';
+
+// ----- Технічні повідомлення ----------
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Перевірка підключення Swiper
+    if (typeof Swiper !== 'undefined') {
+        console.log("Swiper скрипт успішно підключено!");
+    } else {
+        console.error("Swiper скрипт не підключено.");
+    }
+    // Перевірка підключення Accordion
+    if (typeof Accordion !== 'undefined') {
+        console.log("Accordion скрипт успішно підключено!");
+    } else {
+        console.error("Accordion скрипт не підключено.");
+    }
+});
