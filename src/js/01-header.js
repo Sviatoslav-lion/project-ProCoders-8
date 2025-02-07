87db072c06375524eb10dddf72779e90ac3196bc
oleh <olegduzh@gmail.com>
2025-02-07 17:48:14 +0200

    before pull

--- src/js/01-header.js
+++ src/js/01-header.js
@@ -1,44 +1,19 @@
-// // Перевірка підключення Accordion
-// console.log("01-header скрипт успішно підключено!");
-document.addEventListener("DOMContentLoaded", () => {
-  const menuBtn = document.querySelector(".menu-desk-tabl-btn");
-  const nav = document.querySelector(".header-nav");
-  const burgerMenu = document.querySelector(".burger-menu");
-  const mobileMenu = document.querySelector(".mobile-menu");
-  const closeMenu = document.querySelector(".close-menu");
-  const navLinks = document.querySelectorAll('.header-menu a');
+const menuButton = document.querySelector(".menu-desk-tabl-btn");
+const menuList = document.querySelector(".header-menu");
 
-  // Відкриття/закриття десктопної навігації
-  menuBtn.addEventListener("click", () => {
-    nav.classList.toggle("active");
-  });
+menuButton.addEventListener("click", () => {
+  menuList.classList.toggle("is-open");
+});
+document.querySelectorAll('.header-menu a').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault(); // Забороняємо стандартну поведінку посилання
 
-  // Закриття навігації при кліку за межами меню
-  document.addEventListener("click", event => {
-    if (!menuBtn.contains(event.target) && !nav.contains(event.target)) {
-      nav.classList.remove("active");
+    const targetId = this.getAttribute('href'); // Отримуємо значення href
+    const targetSection = document.querySelector(targetId); // Знаходимо відповідну секцію
+
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: 'smooth' }); // Плавно скролимо
     }
   });
-
-  // Відкриття мобільного меню
-  // burgerMenu.addEventListener("click", () => {
-  //   mobileMenu.classList.add("active");
-  // });
-
-  // Закриття мобільного меню
-  // closeMenu.addEventListener("click", () => {
-  //   mobileMenu.classList.remove("active");
-  // });
-
-  // Плавний скрол
-  navLinks.forEach(link => {
-    link.addEventListener("click", event => {
-      event.preventDefault();
-      const targetId = link.getAttribute("href").substring(1);
-      const targetElement = document.getElementById(targetId);
-      window.scrollTo({ top: targetElement.offsetTop - 50, behavior: "smooth" });
-      nav.classList.remove("active");
-      mobileMenu.classList.remove("active");
-    });
-  });
 });
+
