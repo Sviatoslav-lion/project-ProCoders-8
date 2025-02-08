4e0c11e62eb8bbd86d0bae46b126c705df156328
ang1307 <shaidiuk.angelina@gmail.com>
2025-02-08 19:02:17 +0200

    about me

---
+++ src/js/03-about_me.js
@@ -0,0 +1,74 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Mobile-friendly portfolio section loaded!");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".accordion-item");
+
+    items.forEach((item) => {
+        const header = item.querySelector(".accordion-header");
+
+        header.addEventListener("click", function () {
+            item.classList.toggle("open");
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".swiper", {
+        slidesPerView: 2, // 2 кола на екрані
+        spaceBetween: 10,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+        },
+        keyboard: {
+            enabled: true,
+        },
+        mousewheel: true,
+
+
+        breakpoints: {
+            768: { // Для планшетів 
+                slidesPerView: 3,
+            },
+            1440: { // Для комп'ютерів 
+                slidesPerView: 6,
+            }
+        }
+    });
+});
+
+
