5091759e13df08bd4f85ba8efc8f32086732e5c5
Sviatoslav <sviat.danko@gmail.com>
2025-02-10 22:37:05 +0200

    fix bug about me id + h2

--- src/js/03-about_me.js
+++ src/js/03-about_me.js
@@ -20,28 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-/*
-document.addEventListener("DOMContentLoaded", function () {
-    const faqItems = document.querySelectorAll(".accordion-about-me");
-  
-    faqItems.forEach((item) => {
-      const faqAnswer = item.querySelector(".accordion-content");
-      const faqQuestion = item.querySelector(".accordion-header");
-      const faqIcon = item.querySelector(".aboutme-open-icon");
-  
-      faqAnswer.style.display = "none";
-      faqIcon.style.transform = "rotate(0deg)";
-  
-      faqQuestion.addEventListener("click", function () {
-        const isOpen = faqAnswer.style.display === "block";
-  
-        faqAnswer.style.display = isOpen ? "none" : "block";
-        faqQuestion.classList.toggle("active", !isOpen);
-        faqIcon.style.transform = isOpen ? "rotate(0deg)" : "rotate(180deg)";
-      });
-    });
-  });
-  */
+
 
 
 
