9e72a87933a743f36c5ff0667480e572d6848499
Sviatoslav <sviat.danko@gmail.com>
2025-02-10 19:58:17 +0200

    left box

--- src/js/03-about_me.js
+++ src/js/03-about_me.js
@@ -1,31 +1,47 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Mobile-friendly portfolio section loaded!");
-});
+    const aboutItems = document.querySelectorAll(".accordion-item");
 
 
+    aboutItems.forEach((item) => {
+        const aboutmeHeader = item.querySelector(".accordion-header");
+        const aboutmeIcon = item.querySelector(".abme");
+        const aboutmeContent = item.querySelector(".accordion-content");
+        
+        aboutmeContent.style.display = "none";
+        aboutmeIcon.style.transform = "rotate(0deg)";
 
-
-
-
-
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const items = document.querySelectorAll(".accordion-item");
-
-    items.forEach((item) => {
-        const header = item.querySelector(".accordion-header");
-
-        header.addEventListener("click", function () {
-            item.classList.toggle("open");
+        aboutmeHeader.addEventListener("click", function () {
+            //item.classList.toggle("open");                      // відкриваємо прихований вміст елементу
+            const isOpen = aboutmeContent.style.display === "block";     // якщо відкрили, тоді присвоємо змінній значення видимості  
+            aboutmeContent.style.display = isOpen ? "none" : "block";
+            aboutmeHeader.classList.toggle("active", !isOpen);
+            aboutmeIcon.style.transform = isOpen ? "rotate(0deg)" : "rotate(180deg)";
         });
     });
 });
 
-
+/*
+document.addEventListener("DOMContentLoaded", function () {
+    const faqItems = document.querySelectorAll(".accordion-about-me");
+  
+    faqItems.forEach((item) => {
+      const faqAnswer = item.querySelector(".accordion-content");
+      const faqQuestion = item.querySelector(".accordion-header");
+      const faqIcon = item.querySelector(".aboutme-open-icon");
+  
+      faqAnswer.style.display = "none";
+      faqIcon.style.transform = "rotate(0deg)";
+  
+      faqQuestion.addEventListener("click", function () {
+        const isOpen = faqAnswer.style.display === "block";
+  
+        faqAnswer.style.display = isOpen ? "none" : "block";
+        faqQuestion.classList.toggle("active", !isOpen);
+        faqIcon.style.transform = isOpen ? "rotate(0deg)" : "rotate(180deg)";
+      });
+    });
+  });
+  */
 
 
 
