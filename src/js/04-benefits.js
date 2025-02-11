b3a5cb941049942336a5306aaf53f88796f161d7
Romanenko Yliia(mac) <romany0507306677@gmail.com>
2025-02-11 23:08:52 +0100

    del margin

---
+++ src/js/04-benefits.js
@@ -0,0 +1,5 @@
+
+  document.querySelector(".btn-benefit").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.querySelector("#work").scrollIntoView({ behavior: "smooth" });
+  });
